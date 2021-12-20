e2d14b6f4df371cd70ca70c3230b728ea1047b0c
douglassmoss <douglassmoss@icloud.com>
2021-12-20 11:32:33 -0800

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //As a user, I should be able to type a task into the input field.
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createToDo(e.target.new_task_description.value);
+    form.reset();
+  });
+  function createToDo(todo) {
+    //As a user, I expect to see the task string that I provided appear
+    let p = document.createElement("p");
+    let btn = document.createElement("button");
+    btn.addEventListener("click", handleDeleteTask);
+    btn.textContent = " X";
+
+    p.textContent = `${todo} `;
+    //Input to DOM after the submit button has been activated.*/
+    document.querySelector("#tasks").appendChild(p);
+    //adding a delete button to task
+    p.appendChild(btn);
+  }
 });
+function handleDeleteTask(e) {
+  e.target.parentNode.remove();
+}
